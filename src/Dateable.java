def9d45d0f77e34088c30e13478c050b6bf60d40
12ryn <zryn1212@gmail.com>
2023-09-12 18:21:33 +0800

    Ryan's 1.1 Labs

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner ageScanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = ageScanner.nextInt();
+        int dateableAge = age / 2 + 7;
+        System.out.println(age + "-year olds should date somebody who is at least " + dateableAge + " years old");
+        System.out.println("Thanks to Jim Cohoon at The University of Virginia");
+
     }
 }
